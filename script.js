c1df4a16df04817f9a5d9743f492bf9c7d556596
HALOREL67 <adhitamafarrel9@gmail.com>
2026-08-28 14:04:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Website kelas berhasil dimuat!");
+let lastScroll = 0;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll > 80 && currentScroll > lastScroll) {
+        // Scroll ke bawah
+        header.classList.add("minimized");
+    } else if (currentScroll < lastScroll) {
+        // Scroll ke atas
+        header.classList.remove("minimized");
+    }
+
+    lastScroll = currentScroll;
+});

109847e319a7c7352bb6431af071d98dcb6651a8
HALOREL67 <adhitamafarrel9@gmail.com>
2026-08-28 14:22:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
     const currentScroll = window.scrollY;
 
-    if (currentScroll > 80 && currentScroll > lastScroll) {
+    if (currentScroll > lastScroll && currentScroll > 80) {
         header.classList.add("minimized");
     } else if (currentScroll < lastScroll) {
         header.classList.remove("minimized");
